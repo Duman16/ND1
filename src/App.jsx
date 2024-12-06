8fb44bbd0970b76d518bd1bcb9f1085c11b37674
Duman <duman29042006@gmail.com>
2024-12-07 03:18:38 +0500

    my last update to my project

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/header';
 import RandomPlanet from './components/random-planet';
 import People from './components/people';
 import './styles/styles.css';
 
-
 const App = () => {
   return (
-    <div>
+    <Router>
       <Header />
-      <RandomPlanet />
-      <People />
-    </div>
+      <Routes>
+        <Route path="/planets" element={<RandomPlanet />} />
+        <Route path="/people" element={<People />} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/components/Starships.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Starships = () => {
+  return (
+    <div>
+      <h2>Starships Page</h2>
+      <p>Here you can see a list of starships.</p>
+    </div>
+  );
+};
+
+export default Starships;
